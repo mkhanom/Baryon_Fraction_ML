f8dd8c134682bc1e335abb911992b450dc260456
mkhanom <112966228+mkhanom@users.noreply.github.com>
2025-04-08 23:02:46 -0500

    Update gen_rf_trained.py
    Fix ValueError caused by large/infinite values

--- gen_rf_trained.py
+++ gen_rf_trained.py
@@ -3,7 +3,7 @@ import pickle
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-from preprocess_data import *
+from process_data import *
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import r2_score
 from interpret.glassbox import ExplainableBoostingRegressor
@@ -12,10 +12,19 @@ from sklearn.ensemble import RandomForestRegressor
 
 if __name__ == "__main__":
     X_train, X_test, y_train, y_test = get_train_test_data(log_data=False)
-    X_train = np.nan_to_num(X_train, nan=0.0, posinf=0.0, neginf=0.0)
+    
     # Initialize RandomForestRegressor with specified parameters
     rf = RandomForestRegressor(n_estimators=300, max_depth=30, min_samples_split=15,
                          min_samples_leaf=6, max_samples=0.75, random_state=42, n_jobs=9)
+
+    #  X_train is float64
+    X_train = X_train.astype(np.float64)
+
+    # Replace infs 
+    X_train.replace([np.inf, -np.inf], 0, inplace=True)
+
+    # Cap very large/small values to avoid float32 overflow
+    X_train = X_train.clip(-1e15, 1e15)
 
     # Train the model on the entire training dataset
     rf.fit(X_train, y_train)
